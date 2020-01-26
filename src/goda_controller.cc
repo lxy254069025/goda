a39148cdc9b1a1172deda3df42778c74c5ee9b51
Lucas <15580835287@163.com>
2020-01-26 13:27:39 +0800

    readme

--- src/goda_controller.cc
+++ src/goda_controller.cc
@@ -93,6 +93,7 @@ ZEND_METHOD(goda_controller, render) {
     if (saveData) {
         assgin = zend_read_property(goda_controller_ce, getThis(), ZEND_STRL(GODA_CONTROLLER_ASSGIN), 1, NULL);
         if (val) {
+            zval_add_ref(val);
             zend_hash_merge(Z_ARRVAL_P(assgin), Z_ARRVAL_P(val), zval_add_ref, 0);
         }
 
